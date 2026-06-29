442dd36b89c7871061749b66f7b509cada490d65
exussum <exussum@gmail.com>
2026-06-29 05:02:41 -0400

    Use pip instead of uv in install.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -6,9 +6,7 @@ if [ -n "$(git status --porcelain)" ]; then
     exit 1
 fi
 
-printf 'SHA = "%s"\nBUILD_TIME = "%s"\n' "$(git rev-parse --short HEAD)" "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" > src/arrow/_build.py
 rm -rf dist
 uv pip install '.[build]'
 uv build --wheel
 uv run --no-sync twine upload -u a -p a --repository-url http://registry.int.exussum.org dist/arrow-*.whl
-git checkout src/arrow/_build.py
